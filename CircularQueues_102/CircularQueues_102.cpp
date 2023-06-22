b0393eb7239fc15f34d452a62c4b09a28a29af48
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-22 14:54:56 +0700

    Add project files.

---
+++ CircularQueues_102/CircularQueues_102.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Queues {						//membuat class queues
+	int FRONT, REAR, max = 5;
+	int queues_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+	}
+};
+int main()
+{
+
+}

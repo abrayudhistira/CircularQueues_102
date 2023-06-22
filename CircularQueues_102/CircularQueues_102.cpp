5a454a779bb459a0c0939b1fe4b0b6ee86754451
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-22 15:50:24 +0700

    while program

--- CircularQueues_102/CircularQueues_102.cpp
+++ CircularQueues_102/CircularQueues_102.cpp
@@ -95,10 +95,42 @@ int main() {				// main program
 	Queues q;
 	char ch;
 
-	while (true) {
+	while (true) {			// while program
 		try {
 			cout << "Menu" << endl;
 			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display Values" << endl;
+			cout << "4. Enter your choices (1-4) : ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return;
+			}
+			default: {
+				cout << "Invalid options!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& a) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
+
+	return 0;
 }

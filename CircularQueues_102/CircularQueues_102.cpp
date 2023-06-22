fc55c751986a6f82925ea4b910e1674db4ef6214
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-22 15:00:38 +0700

    cek apakah antrian penuh

--- CircularQueues_102/CircularQueues_102.cpp
+++ CircularQueues_102/CircularQueues_102.cpp
@@ -18,6 +18,10 @@ public:
 		cout << endl;
 
 		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue Overflow\n";
+			return;
+		}
 	}
 };
 int main()

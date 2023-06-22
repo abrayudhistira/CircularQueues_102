f941894c27c098378460be02f6df352e52017038
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-22 15:18:59 +0700

    JIka elemen yang dihapus berada di posisi terakhir array, kembali ke awal array

--- CircularQueues_102/CircularQueues_102.cpp
+++ CircularQueues_102/CircularQueues_102.cpp
@@ -51,6 +51,13 @@ public:									//constructor
 			FRONT = -1;
 			REAR = -1;
 		}
+		else {
+			//JIka elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 };
 int main()
